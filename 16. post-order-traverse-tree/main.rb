06c2f8ab6896f9882b4dfd21f22708b94ed74ca0
DSMdongly <gdh0608@naver.com>
2018-04-07 00:34:47 +0900

    added '16. post order traversing tree' and remove '17.traversing tree'

--- 16. post-order-traverse-tree/main.rb
+++ 16. post-order-traverse-tree/main.rb
@@ -1,7 +1,7 @@
-# 14. 이진 트리의 전위 순회
+# 16. 이진 트리의 후위 순회
 
 # 이진 트리에서 root, left, right 노드가 있을 때
-# root -> left -> right 순서로 트리를 순회
+# left -> right -> root 순서로 트리를 순회
 
 # - ex -
 
@@ -16,10 +16,10 @@
 #  B C
 # D   E
 
-# 그리고 rott -> left -> right 순서로 노드를 방문해
+# 그리고 left -> right -> root 순서로 노드를 방문해
 # ↓ 같은 출력을 구하자
 
-# A B D C E
+# D B E C A
 
 
 # 트리를 구현하기 위한 이중 연결 리스트 객체
@@ -88,52 +88,71 @@ def get_tree_from_dual_linked_list(len)
     return nodes[0]
 end
 
-# 트리를 전위 순회하는 함수
-# root, left, right 순서로 노드를 방문
-def pre_order_traverse_tree(root)
+# 트리를 후위 순회하는 함수
+# left, right, root 순서로 노드를 방문
+def post_order_traverse_tree(root)
     # 현재 노드의 부모 노드를 추적하기 위한 스택(배열)
     stack = []
 
-    # 현재 루트 노드 (current root node)
+    # 현재 탐색의 기준이 되는 노드 (current pivot node from traversing)
     cnode = root
- 
-    # 좌측 자식 노드를 탐색하는 바깥쪽 루프
+    
     loop do
-        # stack에 노드들을 저장
+        # 현재 노드의 우측 자식 노드가 존재할 경우
+        # 스택에 우측 자식 노드를 저장
+        rnode = cnode.right
+        stack.push rnode unless rnode.nil?
+
+        # 이후 현재 노드를 스택에 저장
         stack.push cnode
- 
-        # 현재 루트 노드를 먼저 방문
-        print "#{cnode.data} "
- 
-        # 우선 좌측의 자식 노드를 추적
-        unless cnode.left.nil?
-            # cnode를 좌측 자식 노드로 설정함으로써
-            # 자식 노드의 좌측 자식 노드 추적
-            cnode = cnode.left
+
+        # 현재 노드의 좌측 자식 노드가 존재할 경우
+        # 좌측 자식 노드의 탐방 시작
+        lnode = cnode.left
+
+        unless lnode.nil?
+            # 좌측 자식 노드가 존재할 경우
+            # 탐색할 노드를 좌측 자식 노드로 설정
+            cnode = lnode
             next
         end
- 
-        # 우측 자식 노드를 탐색하는 안쪽 루프
+        
         loop do
-            # 스택 최상위에 저장한 노드를 꺼냄
+            # 스택에 저장해 둔 기준 노드를 꺼냄
             cnode = stack.pop
- 
-            # 스택이 빌 경우는 루트 노드까지 순회가 끝났다는 것
-            # 따라서 추적을 중지
             break if cnode.nil?
- 
-            # 우측 노드 탐색
-            unless cnode.right.nil?
-                cnode = cnode.right
-                break
+
+            # 우측 자식 노드가 존재하고 스택의 최상위에 노드가 존재할 경우 
+            # 우측 자식 노드의 탐색이 끝나지 않았으므로 새로이 탐색 시작
+            rnode = cnode.right
+            tnode = stack.last
+
+            unless rnode.nil? || tnode.nil?
+                # 최상위 노드가 우측 자식 노드가 아닐 경우
+                # 탐색을 수행하지 않음
+                if rnode == tnode
+                    # 스택에서 우측 노드를 꺼내고
+                    # 현재 기준 노드는 다시 집어넣음
+                    pnode = stack.pop
+                    stack.push cnode
+
+                    cnode = pnode
+                    break
+                end
             end
+
+            # 현재 기준 노드를 출력
+            print "#{cnode.data} "
         end
-         
-        # 현재 루트 노드가 nil, 추적이 끝날 경우
-        # 바깥쪽 루프도 중단
+
         break if cnode.nil?
     end
- 
+
     # 단순한 줄바꿈
     puts
 end
+
+len = Integer(gets)
+root = get_tree_from_dual_linked_list(len)
+
+post_order_traverse_tree(root)
